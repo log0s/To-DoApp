fc55126063b628ac038a6424f12ba6a98330e483
log0s <JewishRhino@gmail.com>
2013-10-08 21:49:00 -0600

    JS refactoring
    Changed jQuery selector for better future functionality

--- mytodo.js
+++ mytodo.js
@@ -20,14 +20,14 @@ var app = {
             if (ev.which === 13) {
                 $toDoItem
                     .clone()
-                    .html(app.getItem + '<span class="remove glyphicon glyphicon-remove"></span>')
+                    .html(app.getItem() + '<span class="remove glyphicon glyphicon-remove"></span>')
                     .appendTo($items);
             }
         },
         
         removeItem: function(ev) {
             $(ev.target)
-                .parent()
+                .closest('li')
                 .remove();
         }
 });
