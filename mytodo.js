5b856ae0985ef17b8408d7dc21b901eaa0b73791
log0s <JewishRhino@gmail.com>
2013-10-08 21:43:26 -0600

    Initial JS work
    Began writing functions and creating functionality for app

--- mytodo.js
+++ mytodo.js
@@ -1,6 +1,33 @@
-$(function() 
-{
-    $('#itemEntry').keydown(function(event)
-        {
-            
-};
+var app = {
+    init: function() {
+        //Create jQuery objects for commonly used DOM elements and default to-do element
+        var $itemEntry = $('#itemEntry'),
+            $items = $('#items'),
+            $statusBar = $('#statusBar'),
+            $remaining = $('#remaining'),
+            $selector = $('#selector'),
+            $toDoItem = $('<li class="todo list-group-item"></li>');
+        
+        $itemEntry.keydown(app.addItem);
+        $items.on('click', '.remove', app.removeItem);
+        },
+        
+        getItem: function() {
+            return $itemEntry.val();
+        },
+        
+        addItem: function(ev) {
+            if (ev.which === 13) {
+                $toDoItem
+                    .clone()
+                    .html(app.getItem + '<span class="remove glyphicon glyphicon-remove"></span>')
+                    .appendTo($items);
+            }
+        },
+        
+        removeItem: function(ev) {
+            $(ev.target)
+                .parent()
+                .remove();
+        }
+});
