95283a503d2f5e9480f2048e9ff66c2926131082
log0s <JewishRhino@gmail.com>
2013-10-09 13:06:58 -0600

    JS work and fixes
    Shifted variable declaration position in order to put them in the proper
    scope. Added function to track number of items left and fixed getItem to
    properly reset the input field after retrieving the input

--- mytodo.js
+++ mytodo.js
@@ -1,33 +1,60 @@
+//Create jQuery objects for commonly used DOM elements and default to-do element
+var $itemEntry = $('#itemEntry'),
+    $items = $('#items'),
+    $statusBar = $('#statusBar'),
+    $remaining = $('#remaining'),
+    $selector = $('#selector'),
+    $toDoItem = $('<li class="todo list-group-item"></li>');
+
+//Object to contain all app functions
 var app = {
-    init: function() {
-        //Create jQuery objects for commonly used DOM elements and default to-do element
-        var $itemEntry = $('#itemEntry'),
-            $items = $('#items'),
-            $statusBar = $('#statusBar'),
-            $remaining = $('#remaining'),
-            $selector = $('#selector'),
-            $toDoItem = $('<li class="todo list-group-item"></li>');
-        
+    init: function() {        
         $itemEntry.keydown(app.addItem);
         $items.on('click', '.remove', app.removeItem);
         },
         
-        getItem: function() {
-            return $itemEntry.val();
-        },
+    getItem: function() {
+        var text = $itemEntry.val();
         
-        addItem: function(ev) {
-            if (ev.which === 13) {
-                $toDoItem
-                    .clone()
-                    .html(app.getItem() + '<span class="remove glyphicon glyphicon-remove"></span>')
-                    .appendTo($items);
-            }
-        },
+        $itemEntry.val('');
         
-        removeItem: function(ev) {
-            $(ev.target)
-                .closest('li')
-                .remove();
+        return text;
+    },
+        
+    addItem: function(ev) {
+        if (ev.which === 13) {
+            $toDoItem
+                .clone()
+                .html(app.getItem() + '<span class="remove glyphicon glyphicon-remove"></span>')
+                .appendTo($items);
         }
+        
+        app.updateCount();
+    },
+        
+    removeItem: function(ev) {
+        $(ev.target)
+            .closest('li')
+            .remove();
+        
+        app.updateCount();
+    },
+    
+    updateCount: function() {
+        var length = $items.find('li').length,
+            count = '';
+        
+        if (length === 1) {
+            count = '1 item left';
+        }
+        else if (length > 1) {
+            count = length.toString() + ' items left';
+        }
+        
+        $remaining.text(count);
+    }
+};
+
+$(function() {
+    app.init();
 });
