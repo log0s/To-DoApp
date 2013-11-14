8efffa2dac325078d8a83472b6d80892f62f2c90
log0s <JewishRhino@gmail.com>
2013-11-13 18:59:52 -0700

    Added sinon and continued testing

--- tests.js
+++ tests.js
@@ -1,4 +1,23 @@
 describe('To-Do App Tests', function() {
     describe('Add Item Function', function() {
+        
+        beforeEach(function() {
+            $('body')
+                .append('<input id="itemEntry" value="Test item">')
+                .append('<ul id="items">');
+        });
+        
+        afterEach(function() {
+            $('#itemEntry, #items').remove();
+        });
+        
         it('should add styled items to the list', function() {
-            app.item.add
+            app.item.add( { which: 13 } );
+            
+            var $todo = $('#items.todo');
+            
+            expect($todo.length).to.equal(1);
+            expect($todo.find('label').text()).to.equal('Test item');
+        });
+    });
+});
