c064f43630117a0bf5bead8864cec81d3aaeb449
log0s <JewishRhino@gmail.com>
2013-11-13 19:23:32 -0700

    Playing with sinon.spy

--- tests.js
+++ tests.js
@@ -7,17 +7,24 @@ describe('To-Do App Tests', function() {
                 .append('<ul id="items">');
         });
         
-        afterEach(function() {
-            $('#itemEntry, #items').remove();
-        });
-        
         it('should add styled items to the list', function() {
+            var spyV = sinon.spy(app.update, 'visibility'),
+                spyR = sinon.spy(app.update, 'remaining');
+            
             app.item.add( { which: 13 } );
             
             var $todo = $('#items.todo');
             
             expect($todo.length).to.equal(1);
-            expect($todo.find('label').text()).to.equal('Test item');
+            expect($todo.find('toDoText').text()).to.equal('Test item');
+            
+            expect(spyV.callCount).to.equal(1);
+            expect(spyR.callCount).to.equal(1);
+        });
+        
+                
+        afterEach(function() {
+            $('#itemEntry, #items').remove();
         });
     });
 });
